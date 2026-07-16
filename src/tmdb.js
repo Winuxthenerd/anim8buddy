5211b30c71ba8f3473374b704f701a32a62aec37
Winuxthenerd <rowlandswinston0@email.com>
2026-07-16 13:15:39 +0100

    fixed the posters for series

--- src/tmdb.js
+++ src/tmdb.js
@@ -4,14 +4,26 @@ const TMDB_IMAGE_BASE = 'https://image.tmdb.org/t/p/w500'
 
 export async function getMoviePoster(title, year) {
   try {
-    const response = await fetch(
+    // Try movies first
+    const movieRes = await fetch(
       `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(title)}&year=${year}`
     )
-    const data = await response.json()
-    if (data.results && data.results.length > 0) {
-      const poster = data.results[0].poster_path
+    const movieData = await movieRes.json()
+    if (movieData.results && movieData.results.length > 0) {
+      const poster = movieData.results[0].poster_path
       if (poster) return `${TMDB_IMAGE_BASE}${poster}`
     }
+
+    // If no movie found, try TV series
+    const tvRes = await fetch(
+      `${TMDB_BASE_URL}/search/tv?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(title)}&first_air_date_year=${year}`
+    )
+    const tvData = await tvRes.json()
+    if (tvData.results && tvData.results.length > 0) {
+      const poster = tvData.results[0].poster_path
+      if (poster) return `${TMDB_IMAGE_BASE}${poster}`
+    }
+
     return null
   } catch (error) {
     return null
